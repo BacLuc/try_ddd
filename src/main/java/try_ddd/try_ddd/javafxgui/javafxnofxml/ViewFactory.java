6c5eb827c367d8127cea679fbbf1d62d815e9d09
BacLuc <lucius.bachmann@gmx.ch>
2017-05-12 12:25:06 +0200

    use reflection instead of if statement

--- src/main/java/try_ddd/try_ddd/javafxgui/javafxnofxml/ViewFactory.java
+++ src/main/java/try_ddd/try_ddd/javafxgui/javafxnofxml/ViewFactory.java
@@ -4,19 +4,20 @@ import try_ddd.try_ddd.javafxgui.javafxnofxml.views.View1;
 import try_ddd.try_ddd.javafxgui.javafxnofxml.views.View2;
 import javafx.scene.Node;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Created by lucius on 12.05.17.
  */
 public class ViewFactory {
 
-    public AbstractView getView(Class viewclass){
-        if(viewclass.equals(View1.class)) return new View1();
-        if(viewclass.equals(View2.class)) return new View2();
-
-        throw new IllegalArgumentException(String.format("%s not supported by %s",viewclass,getClass().getName() ));
+    public AbstractView getView(Class viewclass) throws Exception {
+        Constructor constructor = viewclass.getConstructor();
+        return (AbstractView) constructor.newInstance();
     }
 
-    public Node getNodeOfView(Class viewclass){
+    public Node getNodeOfView(Class viewclass) throws Exception {
         return getView(viewclass).getRoot();
     }
 }
