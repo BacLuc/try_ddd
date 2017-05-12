42631c9b67ce9162f9a6374d70558c6ca2130e3c
BacLuc <lucius.bachmann@gmx.ch>
2017-05-12 14:41:33 +0200

    found fun fact with concurrentmodification

--- src/main/java/listtest/LanguageList.java
+++ src/main/java/listtest/LanguageList.java
@@ -20,12 +20,7 @@ public class LanguageList {
     }
 
     public void remove(String name){
-        Language language = new Language(name);
-
-
-        int size = languageList.size();
-
-        Iterator<Language> iterator= languageList.iterator();
+       Language language = new Language(name);
 
        for(Language lang : languageList){
            if(lang.equals(language)){
@@ -33,6 +28,58 @@ public class LanguageList {
            }
        }
     }
+
+    public void removeWithIterator(String name){
+        Language language = new Language(name);
+
+
+        int size = languageList.size();
+
+        Iterator<Language> iterator= languageList.iterator();
+
+        while(iterator.hasNext()){
+            Language lang = iterator.next();
+            if(lang.equals(language)){
+                languageList.remove(lang);
+            }
+        }
+    }
+
+    public void removeWithIteratorAndIndex(String name){
+        Language language = new Language(name);
+
+
+        Iterator<Language> iterator= languageList.iterator();
+
+        while(iterator.hasNext()){
+            Language lang = iterator.next();
+            if(lang.equals(language)){
+                languageList.remove(languageList.indexOf(lang));
+            }
+        }
+    }
+
+    public void removeWithContains(String name){
+        Language language = new Language(name);
+
+        while(languageList.contains(language)){
+            languageList.remove(language);
+        }
+    }
+
+    public void removeWithFor(String name){
+        Language language = new Language(name);
+
+
+        for(int i = 0; i<languageList.size();i++){
+            if(language.equals(languageList.get(i))){
+                languageList.remove(i);
+
+            }
+        }
+    }
+
+
 
     public String toString(){
 
--- src/main/java/listtest/ListTestMain.java
+++ src/main/java/listtest/ListTestMain.java
@@ -4,14 +4,26 @@ package listtest;
  * Created by lucius on 12.05.17.
  */
 public class ListTestMain {
+    private static String[] languages = new String[]{
+            "Java", "Ruby", "Pascal", "C++", "C#", "Javascript", "Go", "Groovy", "PHP", "ObjectiveC", "Perl", "Python", "Scala", "R", "Fritz", "Hans"
+    };
 
     public static void main(String[] args){
+        long starttime;
+
+        int repetitions = 10000;
+
+
+
         LanguageList list = new LanguageList();
 
         list.add("Java");
         list.add("PHP");
         list.add("Ruby");
         list.add("Pascal");
+//        list.add("test");
+//        list.add("Passetecal");
+//        list.add("set");
 
         System.out.println("First test: "+ list.toString());
 
@@ -20,6 +32,103 @@ public class ListTestMain {
 
 
 
+
+
+        starttime = System.currentTimeMillis();
+
+        for(int i=0;i<repetitions;i++) {
+
+            //first test foreach
+             list = getFilledList();
+
+            list.removeWithFor("Ruby");
+        }
+
+
+
+        System.out.println(String.format("Test with removeWithFor remove took: %s",System.currentTimeMillis()-starttime));
+
+
+//
+//        starttime = System.currentTimeMillis();
+//
+//        for(int i=0;i<repetitions;i++) {
+//
+//            //first test foreach
+//            LanguageList list = getFilledList();
+//
+//            list.remove("Ruby");
+//        }
+//
+//
+//
+//        System.out.println(String.format("Test with forEach remove took: %s",System.currentTimeMillis()-starttime));
+
+
+
+//        starttime = System.currentTimeMillis();
+//
+//        for(int i=0;i<repetitions;i++) {
+//
+//            LanguageList list = getFilledList();
+//
+//            list.removeWithIterator("Ruby");
+//        }
+//
+//
+//
+//        System.out.println(String.format("Test with removeWithIterator took: %s",System.currentTimeMillis()-starttime));
+
+
+//        starttime = System.currentTimeMillis();
+//
+//        for(int i=0;i<repetitions;i++) {
+//
+//            //first test foreach
+//            LanguageList list = getFilledList();
+//
+//            list.removeWithIteratorAndIndex("Ruby");
+//        }
+//
+//
+//
+//        System.out.println(String.format("Test with normal removeWithIteratorAndIndex took: %s",System.currentTimeMillis()-starttime));
+//
+
+        starttime = System.currentTimeMillis();
+
+        for(int i=0;i<repetitions;i++) {
+
+            //first test foreach
+             list = getFilledList();
+
+            list.removeWithContains("Ruby");
+        }
+
+
+
+        System.out.println(String.format("Test with normal removeWithContains took: %s",System.currentTimeMillis()-starttime));
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    public static LanguageList getFilledList(){
+        LanguageList list = new LanguageList();
+        for(int i=0; i<1;i++){
+            for(String name : languages){
+                list.add(name);
+            }
+        }
+        return list;
     }
 
 }
